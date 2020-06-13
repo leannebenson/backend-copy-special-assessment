2ab8970438943ebfe06095c1e8d586ae324c96e2
Leanne Benson <stylistleannerowe@gmail.com>
2020-06-13 14:46:10 -0400

    achieving zz__something__.jpg xyz__hello__.txt return in command line for python copyspecial.py . command

--- copyspecial.py
+++ copyspecial.py
@@ -7,7 +7,7 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 
 # give credits
-__author__ = "???"
+__author__ = "LeanneBenson"
 
 import re
 import os
@@ -20,30 +20,44 @@ import argparse
 def get_special_paths(dirname):
     """Given a dirname, returns a list of all its special files."""
     list_files = []
-    path = os.listdir(dirname) #could need to be path_list
-    for file_name in path:
-        match = re.search(r'__(\w+)__', file_name)
+    files = os.listdir(dirname) #could need to be path_list
+    for file in files:
+        match = re.search(r'__(\w+)__', file)
         if match:
-            list_files.append(os.path.abspath(os.path.join(dirname, file_name)))
+            list_files.append(file)
+            # list_files.append(os.path.abspath(os.path.join(dirname, file)))
     return list_files
 
 
-def copy_to(path_list, dest_dir):
+def copy_to(path, files):
 
-    if not os.path.exists(dest_dir):
-        os.mkdir(dest_dir)
-    for path in path_list:
-        file_name = os.path.basename(path)
-        shutil.copy(path, os.path.join(dest_dir, file_name))
+    if not os.path.exists(path):
+        os.mkdir(path)
+    else:
+        print("This path exists")
+
+    for file in files:
+        shutil.copy(file, path)
+        #file_name = os.path.basename(path)
+        #shutil.copy(path, os.path.join(dest_dir, file_name))
     # could also error out if path_list doesnt match up with 
     # path variable from get_special_paths
 
 
-def zip_to(path_list, dest_zip):
+def zip_to(path, dest_zip):
     # your code here
-    # command = "zip -j {} {}".format(zippath, ' '.join(paths)) #thanks Doug
+    paths = list(path)
+    command = "zip -j {} {}".format(dest_zip, ' '.join(paths)) #thanks Doug
+    print("Command I am going to do: ")
+    print(command)
+    os.system(command)
+    #return
 
-    return
+def zip_to_two(paths, zippath):
+    command = ["zip", "-j", zippath]
+    command.extend(paths)
+    print("command I am going to do: {}".format(' '.join(command)))
+    subprocess.check_output(command)
 
 
 def main(args):
@@ -52,6 +66,7 @@ def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
+    parser.add_argument('fromdir', help='src dir to look for local files')
     # TODO: add one more argument definition to parse the 'from_dir' argument
     ns = parser.parse_args(args)
 
@@ -62,6 +77,20 @@ def main(args):
     # This is input data validation. If something is wrong (or missing) with
     # any required args, the general rule is to print a usage message and
     # exit(1).
+    all_paths = get_special_paths(ns.fromdir)
+
+    if ns.todir:
+        copy_to(ns.todir, all_paths)
+
+    if ns.tozip:
+        zip_to(all_paths, ns.tozip)
+    
+    if not ns.todir and not ns.tozip:
+        print('\n'.join(all_paths))
+        # for file in all_paths:
+        #     print(os.path.abspath(file))
+    
+    
 
     # Your code here: Invoke (call) your functions
 
